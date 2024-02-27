eda935a6f1d0489b3465c3ace1017cce6fdd9d04
aldrich27dev <129347457+aldrich27dev@users.noreply.github.com>
2024-02-28 00:29:02 +0800

    Added Scroll top

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
